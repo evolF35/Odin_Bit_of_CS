0189843a04fd091292907fd08bb0d4ef9e478ef0
evolF35 <maccyair@gmail.com>
2022-10-12 15:48:28 -0700

    finished merge sort exercise

--- mergesort/mergesort.js
+++ mergesort/mergesort.js
@@ -2,13 +2,48 @@
 function mergeSort(array){
 
     if(array.length < 2){
-        return ;
+        return array;
     }
-
     else{
-        
+        let mid = Math.floor(array.length/2);
+        let left = mergeSort(array.slice(0,mid));
+        let right = mergeSort(array.slice(mid));
+        return(merge(left,right));
     }
-
 
 }
-console.log(mergeSort([0,1]));
+
+function merge(arrayA,arrayB){
+
+    let a = 0;
+    let b = 0;
+    let c = 0;
+    let arrayC = [];
+
+    while(a < arrayA.length && b < arrayB.length){
+
+        if(arrayA[a] < arrayB[b]){
+            arrayC[c] = arrayA[a];
+            c = c + 1;
+            a = a + 1;
+        }
+        else{
+            arrayC[c] = arrayB[b];
+            c = c + 1;
+            b = b + 1;
+        }
+    }
+
+    for(; a < arrayA.length; a++){
+        arrayC[c] = arrayA[a];
+        c = c + 1;
+    }
+    for(; b < arrayB.length; b++){
+        arrayC[c] = arrayB[b];
+        c = c + 1;
+    }
+
+    return(arrayC);
+}
+
+console.log(mergeSort([1,23,43,22,4,1,3423,2]));
